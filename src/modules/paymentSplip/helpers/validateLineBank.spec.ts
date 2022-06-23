f3f4adaac31c86bce04203e89012f250790d12ad
Daniel Almeida <daniel.arquiteto@gmail.com>
2022-06-23 08:34:12 -0300

    implement test validadeLine

--- src/modules/paymentSplip/helpers/validateLineBank.spec.ts
+++ src/modules/paymentSplip/helpers/validateLineBank.spec.ts
@@ -1,17 +1,24 @@
+import { AppError } from "../../../utils/appError";
 import { validateLine } from "./validateLine";
 
-let line = "033998216796710102179920050201019289400000028103";
-
-type VLine = {
-  validLine: number;
-};
-
-let validLine = 0;
+let line1 = "033998216796710102179920050201019289400000028103";
+let line2 = "10497271728680011964522840403525390260000117388";
 
 describe("validade line bank", () => {
-  it("shoud be able a valid line", () => {
-    validLine = validateLine(line);
+  it("shoud be able a 47 caracter", () => {
+    expect(validateLine(line2)).toBe(47);
   });
 
-  expect(validLine);
+  it("shoud be able a 48 caracter", () => {
+    expect(validateLine(line1)).toBe(48);
+  });
+});
+
+describe("validade line bank only with numbers", () => {
+  it("shoud be able instanceof AppError", async () => {
+    expect(async () => {
+      let line3 = "1049727172868001196452284040352539026000011738a";
+      await validateLine(line3);
+    }).rejects.toBeInstanceOf(AppError);
+  });
 });
